19b6100302e4a2765e752c28f4cc54e0a5f5635c
Abubakr <thefox5000@gmail.com>
2024-02-03 19:07:51 +0300

    Adjust compare periods methods

--- clsPeriod.h
+++ clsPeriod.h
@@ -7,5 +7,46 @@ private:
 	clsDate _Start;
 	clsDate _End;
 
+public:
+	clsPeriod(clsDate Start, clsDate End)
+	{
+		_Start = Start;
+		_End = End;
+	}
 
+	void setStart(clsDate Start)
+	{
+		_Start = Start;
+	}
+
+	clsDate getStart()
+	{
+		return _Start;
+	}
+
+	__declspec(property(get = getStart, put = setStart)) clsDate Start;
+
+	clsDate getEnd()
+	{
+		return _End;
+	}
+
+	void setEnd(clsDate End)
+	{
+		_End = End;
+	}
+
+	__declspec(property(get = getEnd, put = setEnd)) clsDate End;
+
+	bool AreDatesOverLapping(clsPeriod Period1, clsPeriod Period2)
+	{
+		if (clsDate::CompareTwoDates(Period2.End, Period1.Start) == clsDate::BeforeAfterEqualDate::Before || clsDate::CompareTwoDates(Period2.Start, Period1.End) == clsDate::BeforeAfterEqualDate::After)
+		{
+			return false;
+		}
+		else
+		{
+			return true;
+		}
+	}
 };
